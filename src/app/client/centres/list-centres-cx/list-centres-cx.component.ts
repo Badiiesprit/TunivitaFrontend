36f0bdc3f7eeecf62223b85ea514f241d6153884
Badii Abdelkhalak <106381999+Badiiesprit@users.noreply.github.com>
2023-07-06 14:15:08 +0100

    add page center and home

--- src/app/client/centres/list-centres-cx/list-centres-cx.component.ts
+++ src/app/client/centres/list-centres-cx/list-centres-cx.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
-import { CenterService } from 'src/app/services/client/center/center.service';
-import { Center } from 'src/app/model/center';
+import { Component, OnInit } from '@angular/core';
+import { Service } from 'src/app/model/service';
+import { ServiceService } from 'src/app/services/admin/service/service.service';
+import { HttpClient } from '@angular/common/http';
+import { ImageService } from 'src/app/services/admin/image/image.service';
 import { Image } from 'src/app/model/image';
-import { environment } from 'src/app/environments/environment';
+import { environment } from '../../../environments/environment';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-list-centres-cx',
@@ -10,31 +13,85 @@ import { environment } from 'src/app/environments/environment';
   styleUrls: ['./list-centres-cx.component.scss']
 })
 export class ListCentresCxComponent {
-  public centers : Center[] = new Array(0);
+  services: any[] = [];
+  public motcle: string;
   public baseurl = environment.url;
-  constructor(private centerService : CenterService){}
+  filteredServices: any[] = [];
+  currentPage: number = 1; // Current page number
+  itemsPerPage: number = 8; // Number of items to display per page
+  sortOrder: string = 'asc'; // Sort order for date sorting
+  sort_direction:string= "up";
+
+
+  constructor(
+    private serviceService: ServiceService,
+    private http: HttpClient,
+    private imageService: ImageService
+  ) {}
 
   ngOnInit() {
-    this.centerService.getAll().subscribe(
+    this.serviceService.getAll().subscribe(
       (response) => {
-        console.log(response);
-        console.log(response.result);
-        if (response.result) {
-          this.centers = response.result.map((center: Center) => {
-            let image= center.image[0] as Image;
-            return {
-              ...center,
-              pathimage: image.path // Ajoutez ici le chemin de l'image souhaité
-            };
-          });
-          console.log(this.centers);
-        }
+        this.services = response.services;
+        this.filteredServices = this.services;
+        console.log(this.services);
+      },
+      (error) => {
+        console.error('Error fetching services:', error);
       }
     );
   }
 
-  voirplus(centerId : any){
-
+  search(): void {
+    if (this.motcle) {
+      this.filteredServices = this.services.filter(service =>
+        service.name.toLowerCase().includes(this.motcle.toLowerCase()) ||
+        service.description.toLowerCase().includes(this.motcle.toLowerCase()) ||
+        service.location.toLowerCase().includes(this.motcle.toLowerCase())
+      );
+    } else {
+      this.filteredServices = this.services;
+    }
+    this.currentPage = 1; // Reset current page to 1 after search
   }
+
+  get totalPages(): number {
+    return Math.ceil(this.filteredServices.length / this.itemsPerPage);
+  }
+
+  getPageNumbers(): number[] {
+    return Array.from({ length: this.totalPages }, (_, i) => i + 1);
+  }
+
+
+
+
+  sortServicesByDate(): void {
+    let arrayToSort = this.services;
+
+    if (this.motcle) {
+      arrayToSort = this.filteredServices;
+    }
+
+    arrayToSort.sort((a, b) => {
+      const dateA = new Date(a.date).getTime();
+      const dateB = new Date(b.date).getTime();
+
+      if (this.sortOrder === 'asc') {
+        this.sort_direction = "up";
+        return dateA - dateB;
+      } else {
+        this.sort_direction = "down";
+        return dateB - dateA;
+      }
+    });
+    this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
+  }
+
+  getSortIconClass(): string {
+    return this.sortOrder === 'asc' ? 'asc' : 'desc';
+  }
+
+
 
 }
