f608cb49fee0e85437ccc8f26d503a6364ede001
Badii Abdelkhalak <106381999+Badiiesprit@users.noreply.github.com>
2023-07-08 00:53:44 +0100

    add slider

--- src/app/client/home/home.module.ts
+++ src/app/client/home/home.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { CarouselModule } from 'primeng/carousel';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from "./home.component";
 import {
@@ -15,8 +15,6 @@ import {
   SpinnerModule,
   AlertModule,
   TooltipModule,
-  CarouselModule,
-  CarouselItemComponent,
   BadgeModule
 } from '@coreui/angular';
 
