ae9dc40f2c3f3f22054144706f45489dc0cb9e16
nassreddineEsprit <nassreddine.trigui@esprit.tn>
2023-07-08 04:32:23 +0100

    profileupdateuser

--- src/app/client/user/profile/profile.component.ts
+++ src/app/client/user/profile/profile.component.ts
@@ -1,25 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { TokenService } from 'src/app/services/token/token.service';
+import { HttpHeaders } from '@angular/common/http';
 import jwt_decode from 'jwt-decode';
-import { User } from "../../../model/user"
+import { User } from '../../../model/user';
 import { environment } from 'src/app/environments/environment';
 import { Image } from 'src/app/model/image';
+import { UserService } from '../../../services/admin/user/user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
-
 export class ProfileComponent {
   userId: string;
   decodedToken: any | null;
   user: User = new User();
   public baseurl = environment.url;
-  public image:Image = new Image();
+  public image: Image = new Image();
 
-
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit() {
     const token = localStorage.getItem('token');
@@ -29,10 +29,26 @@ export class ProfileComponent {
 
       this.userId = this.decodedToken.userId;
       console.log(this.userId);
-      this.image = this.user.image as Image;
-
-    } else {
-      console.error('Token is null. Please log in.');
+      if (this.userId) {
+        const headers = new HttpHeaders().set(
+          'Authorization',
+          `Bearer ${token}`
+        );
+        this.userService.getById(this.userId).subscribe(
+          (response) => {
+            if (response.result) {
+              console.log(response.result);
+              this.user = response.result;
+              this.image = this.user.image as Image;
+            }
+          },
+          (error) => {
+            console.error('Error fetching user:', error);
+          }
+        );
+      } else {
+        console.error('Token is null. Please log in.');
+      }
     }
   }
 }
--- src/app/client/user/user-routing.module.ts
+++ src/app/client/user/user-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component'
 import { UpdateComponent } from './update/update.component'
 
+
 const routes: Routes = [
   {
     path: '',
